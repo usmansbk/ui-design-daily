6cb012b97d254d9628fa06e711879f85acb8cc8f
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 00:45:45 +0100

    Style grid

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -1,7 +1,17 @@
+import classes from "./Card.module.css";
+
 export default function Card() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div className={classes.container}>
+      <img
+        src="https://www.uidesigndaily.com/uploads/1444/day_1444_thumb.png"
+        alt=""
+        className={classes.thumbnail}
+      />
+      <div>
+        <h1>Charts</h1>
+        <p>16 Sep 2021</p>
+      </div>
+    </div>
+  );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,24 @@
 import Header from "./Header";
+import Card from "./Card";
 import classes from "./Home.module.css";
 
 export default function Home() {
   return (
     <div className={classes.container}>
       <Header />
+      <Grid />
+    </div>
+  );
+}
+
+function Grid() {
+  return (
+    <div className={classes.grid}>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }

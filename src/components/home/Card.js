8055719bf162cce4bdd0f485a41016a5fdbdb328
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 00:56:06 +0100

    Style cards

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -3,14 +3,16 @@ import classes from "./Card.module.css";
 export default function Card() {
   return (
     <div className={classes.container}>
-      <img
-        src="https://www.uidesigndaily.com/uploads/1444/day_1444_thumb.png"
-        alt=""
-        className={classes.thumbnail}
-      />
-      <div>
-        <h1>Charts</h1>
-        <p>16 Sep 2021</p>
+      <div className={classes.preview}>
+        <img
+          src="https://www.uidesigndaily.com/uploads/1443/day_1443_thumb.png"
+          alt=""
+          className={classes.thumbnail}
+        />
+      </div>
+      <div className={classes.details}>
+        <h4 className={classes.title}>Charts</h4>
+        <p className={classes.date}>16 Sep 2021</p>
       </div>
     </div>
   );

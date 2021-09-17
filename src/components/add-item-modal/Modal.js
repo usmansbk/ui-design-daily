000387e65fdfb3c8d0385c315072a06c42301edb
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 15:20:25 +0100

    Add imputs

--- src/components/add-item-modal/Modal.js
+++ src/components/add-item-modal/Modal.js
@@ -1,3 +1,53 @@
+import classes from "./Modal.module.css";
+
+function RadioButton({ id, value, label }) {
+  return (
+    <div>
+      <input type="radio" id={id} value={value} />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+}
+
+function TextInput({ id, type, label, placeholder, required, min }) {
+  return (
+    <div>
+      <label htmlFor={id}>
+        {label} {required && "*"}
+      </label>
+      <input
+        required={required}
+        min={min}
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+
 export default function Modal() {
-  return <div></div>;
+  return (
+    <section className={classes.container}>
+      <h2>Add new item</h2>
+      <p>What kind of item would you like to add?</p>
+      <div>
+        <RadioButton id="service" value="Service" label="Service" />
+        <RadioButton id="product" value="Product" label="Product" />
+      </div>
+      <TextInput
+        id="name"
+        label="Name"
+        placeholder="Eg. Business Consulting"
+        required
+      />
+      <TextInput
+        id="price"
+        label="Price"
+        placeholder="Enter amount"
+        required
+        type="number"
+        min={0}
+      />
+    </section>
+  );
 }

136fe3de27f51081e8425d81ed058a121ac0e637
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 20:18:17 +0100

    Use transition group

--- src/components/add-item-modal/Modal.js
+++ src/components/add-item-modal/Modal.js
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { Transition } from "react-transition-group";
 import { useState } from "react";
 import classes from "./Modal.module.css";
 import xmark from "./x-mark.svg";
@@ -61,57 +62,90 @@ function Button({ value, type = "button", contained, onClick }) {
 
 export default function Modal({ visible, onDismiss }) {
   const [checked, setChecked] = useState(false);
+  const transitions = {
+    entering: 0,
+    entered: 1,
+    exiting: 0,
+    exited: 0,
+  };
+  const rootTransitions = {
+    entering: 1,
+    entered: 1,
+    exiting: 1,
+    exited: 0,
+  };
+
   return (
-    <div className={clsx(classes.root, visible && classes.visible)}>
-      <div className={classes.modal}>
-        <section className={classes.container}>
-          <button onClick={onDismiss} className={classes.closeButton}>
-            <img src={xmark} alt="" className={classes.icon} />
-          </button>
-          <h2 className={classes.title}>Add new item</h2>
-          <p className={classes.optionLabel}>
-            What kind of item would you like to add?
-          </p>
-          <div className={classes.radioGroup}>
-            <RadioButton
-              value="Service"
-              label="Service"
-              onClick={setChecked}
-              checked={checked === "Service"}
-            />
-            <RadioButton
-              value="Product"
-              label="Product"
-              onClick={setChecked}
-              checked={checked === "Product"}
-            />
+    <Transition
+      in={visible}
+      timeout={{
+        enter: 0,
+        exit: 200,
+      }}
+    >
+      {(state) => (
+        <div
+          className={classes.root}
+          style={{
+            transform: `scale(${rootTransitions[state]})`,
+          }}
+        >
+          <div className={classes.modal}>
+            <section
+              className={classes.container}
+              style={{
+                transform: `scale(${transitions[state]})`,
+              }}
+            >
+              <button onClick={onDismiss} className={classes.closeButton}>
+                <img src={xmark} alt="" className={classes.icon} />
+              </button>
+              <h2 className={classes.title}>Add new item</h2>
+              <p className={classes.optionLabel}>
+                What kind of item would you like to add?
+              </p>
+              <div className={classes.radioGroup}>
+                <RadioButton
+                  value="Service"
+                  label="Service"
+                  onClick={setChecked}
+                  checked={checked === "Service"}
+                />
+                <RadioButton
+                  value="Product"
+                  label="Product"
+                  onClick={setChecked}
+                  checked={checked === "Product"}
+                />
+              </div>
+              <TextInput
+                id="name"
+                label="Name"
+                placeholder="Eg. Business Consulting"
+                required
+              />
+              <TextInput
+                id="description"
+                label="Description"
+                placeholder="Describe your product"
+                multiline
+              />
+              <TextInput
+                id="price"
+                label="Price"
+                placeholder="Enter amount"
+                required
+                type="number"
+                min={0}
+              />
+              <div className={classes.buttonGroup}>
+                <Button value="Cancel" onClick={onDismiss} />
+                <Button value="Add item" contained />
+              </div>
+            </section>
           </div>
-          <TextInput
-            id="name"
-            label="Name"
-            placeholder="Eg. Business Consulting"
-            required
-          />
-          <TextInput
-            id="description"
-            label="Description"
-            placeholder="Describe your product"
-            multiline
-          />
-          <TextInput
-            id="price"
-            label="Price"
-            placeholder="Enter amount"
-            required
-            type="number"
-            min={0}
-          />
-          <div className={classes.buttonGroup}>
-            <Button value="Cancel" onClick={onDismiss} />
-            <Button value="Add item" contained />
-          </div>
-        </section>
-      </div>
-    </div>
+        </div>
+      )}
+    </Transition>
   );
 }

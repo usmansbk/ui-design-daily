f9e9eabe95a08f281ae206f1af9814a1e9f2a318
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 01:19:48 +0100

    Add creator

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <footer className={classes.container}>
       <p className={classes.copyright}>&copy;UIDesignDaily 2021</p>
+      <p className={classes.copyright}>Built by Usman Suleiman Babakolo</p>
     </footer>
   );
 }

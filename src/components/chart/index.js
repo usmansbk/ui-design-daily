f3e1c44f7ac440360778c6f285a227786a056b0a
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 21:08:19 +0100

    Add stub

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,5 +1,10 @@
+import Chart from "./Chart";
 import classes from "./index.module.css";
 
 export default function Charts() {
-  return <div className={classes.container}></div>;
+  return (
+    <div className={classes.container}>
+      <Chart />
+    </div>
+  );
 }

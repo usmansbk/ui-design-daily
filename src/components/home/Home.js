6132dabf3624c9d19478d82db9e5b250a994ca82
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 15:06:52 +0100

    Add routes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,14 @@
-import { Link, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 import Card from "./Card";
 import Footer from "./Footer";
 import classes from "./Home.module.css";
-import AddItemModal from "../add-item-modal";
 
 export default function Home() {
   return (
     <div className={classes.container}>
       <Header />
-      <Switch>
-        <Route path="/add-item-modal">
-          <AddItemModal />
-        </Route>
-        <Route exact path="/">
-          <Grid />
-        </Route>
-      </Switch>
+      <Grid />
       <Footer />
     </div>
   );

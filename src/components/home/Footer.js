57b262d058201ae284c3ed6929366f9341eba353
Usman Suleiman <usmansbk@gmail.com>
2021-09-17 10:44:20 +0100

    Add social links

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedinIn,
+  faAngellist,
+  faDev,
+} from "@fortawesome/free-brands-svg-icons";
 import classes from "./Footer.module.css";
 
 export default function Footer() {
@@ -5,6 +12,20 @@ export default function Footer() {
     <footer className={classes.container}>
       <p>Built by Usman Suleiman Babakolo</p>
       <p className={classes.copyright}>&copy;UIDesignDaily 2021</p>
+      <nav className={classes.nav}>
+        <a href="https://github.com/usmansbk">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </a>
+        <a href="https://www.linkedin.com/in/usmansbk/">
+          <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+        </a>
+        <a href="https://www.linkedin.com/in/usmansbk/">
+          <FontAwesomeIcon icon={faAngellist} size="lg" />
+        </a>
+        <a href="https://dev.to/devusman">
+          <FontAwesomeIcon icon={faDev} size="lg" />
+        </a>
+      </nav>
     </footer>
   );
 }
